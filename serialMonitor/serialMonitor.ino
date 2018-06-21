efea8cb44072296412e2c749a5714cf44480c53a
Nilesh <agnilesh8199@gmail.com>
2018-06-21 14:26:39 +0530

    serialMonitor

---
+++ serialMonitor/serialMonitor.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+  serial.begin(9600);
+  serial.println("Hello World");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
