d8e5da099255f9f1141b5b60f1cf791538badcd4
Nilesh <agnilesh8199@gmail.com>
2018-06-21 14:40:43 +0530

    maths in serial monitor

--- serialMonitor/serialMonitor.ino
+++ serialMonitor/serialMonitor.ino
@@ -1,7 +1,17 @@
+int a = 5;
+int b = 10;
+
 void setup() {
   // put your setup code here, to run once:
-  serial.begin(9600);
-  serial.println("Hello World");
+  
+  Serial.begin(9600);
+  Serial.println("Here's some math");
+  Serial.print("a= ");
+  Serial.println(a);
+  Serial.print("b= ");
+  Serial.println(b);
+  Serial.print("a+b= ");
+  Serial.println(a+b);
 }
 
 void loop() {
