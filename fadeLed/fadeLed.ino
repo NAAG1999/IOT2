a5e0df3ce8ca5637177db33b2d31b87725eac7bd
Nilesh <agnilesh8199@gmail.com>
2018-06-22 13:48:11 +0530

    max to min

--- fadeLed/fadeLed.ino
+++ fadeLed/fadeLed.ino
@@ -7,11 +7,12 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //starting from off to maximum glow
+  //starting from min glow to max glow
   for(int fadeValue =0; fadeValue <=255; fadeValue +=5){
     analogWrite(ledPin, fadeValue);
     delay(30);
   }
+  //fading from max glow to min glow
   for(int fadeValue =255; fadeValue >=0; fadeValue -=5){
     analogWrite(ledPin, fadeValue);
     delay(30);
