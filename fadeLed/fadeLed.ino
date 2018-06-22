5c901d0843016a3d8634812b594a014890e6d91b
Nilesh <agnilesh8199@gmail.com>
2018-06-22 13:46:57 +0530

    comment

--- fadeLed/fadeLed.ino
+++ fadeLed/fadeLed.ino
@@ -7,11 +7,12 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //starting from off to maximum glow
   for(int fadeValue =0; fadeValue <=255; fadeValue +=5){
     analogWrite(ledPin, fadeValue);
     delay(30);
   }
-  for(int fadeValue =255; fadeValue =>0; fadeValue -=5){
+  for(int fadeValue =255; fadeValue >=0; fadeValue -=5){
     analogWrite(ledPin, fadeValue);
     delay(30);
   }
